7a356fb49f7e11a0c56f81e5d3fba4eb2b7957d6
Nicholas VanBuskirk <thestublord@gmail.com>
2024-01-05 09:50:44 -0700

    added todo functionality firebase file and store file

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,27 +1,75 @@
 <script>
-  let todos = ["Learn Svelte", "Learn SvelteKit", "Learn SvelteKit Fullstack"];
+  let todos = [];
+  let currTodo = "";
+  let error = false;
+
+  function addTodo() {
+    error = false;
+    if (!currTodo) {
+      error = true;
+    }
+
+    todos = [...todos, currTodo];
+    currTodo = "";
+  }
+
+  function editTodo(index) {
+    let newTodoList = [...todos].filter((val, i) => {
+      console.log(i, index, i !== index);
+      return i != index;
+    });
+    currTodo = todos[index];
+    todos = newTodoList;
+  }
+
+  function removeTodo(index) {
+    let newTodoList = [...todos].filter((val, i) => {
+      console.log(i, index, i !== index);
+      return i != index;
+    });
+    todos = newTodoList;
+  }
 </script>
 
 <div class="mainContainer">
   <div class="headerContainer">
     <h1>Todo List</h1>
-    <button
-      ><i class="far fa-save"></i>
-      <p>Save</p></button
-    >
+    <div class="headerBtns">
+      <button>
+        <i class="far fa-save"></i>
+        <p>Save</p>
+      </button>
+      <button>
+        <i class="far fa-sign-out-alt"></i>
+        <p>Logout</p>
+      </button>
+    </div>
   </div>
 
   <main>
+    {#if todos.length === 0}
+      <p class="center">No Todos</p>
+    {/if}
     {#each todos as todo, index}
       <div class="todo">
-        {index + 1}. {todo}
+        <p>
+          {index + 1}. {todo}
+        </p>
+        <div class="actions">
+          <button>
+            <i class="far fa-edit" on:click={() => editTodo(index)}></i>
+          </button>
+          <button>
+            <i class="far fa-trash-alt" on:click={() => removeTodo(index)}></i>
+          </button>
+        </div>
       </div>
     {/each}
   </main>
 
-  <div class="enterTodo">
-    <input type="text" placeholder="Enter Todo" />
-    <button>Add</button>
+  <div class={`enterTodo ${error ? "errorBorder" : ""}`}>
+    <input bind:value={currTodo} type="text" placeholder="Enter Todo" />
+    <button on:click={addTodo}>Add</button>
   </div>
 </div>
 
@@ -40,6 +88,11 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
+  }
+
+  .headerBtns {
+    display: flex;
+    gap: 8px;
   }
 
   .headerContainer button {
@@ -75,6 +128,40 @@
     margin-top: 24px;
   }
 
+  .todo {
+    padding: 8px;
+
+    border-radius: 4px;
+    background: transparent;
+    color: white;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+
+  .actions {
+    display: flex;
+    gap: 4px;
+    font-size: 1.2rem;
+    margin-left: auto;
+  }
+
+  .actions button:hover {
+    background: rgba(255, 255, 255, 0.1);
+  }
+
+  .actions button {
+    padding: 8px;
+    border: none;
+    border-radius: 4px;
+    background: transparent;
+    color: white;
+    cursor: pointer;
+    display: flex;
+    gap: 8px;
+    align-items: center;
+  }
+
   .enterTodo {
     display: flex;
     gap: 24px;
@@ -82,9 +169,13 @@
     border: 1px solid white;
   }
 
+  .enterTodo.errorBorder {
+    border: 1px solid red !important;
+  }
+
   .enterTodo input {
     flex: 1;
-    padding: 8px;
+    padding: 14px;
     border: none;
     border-radius: 4px;
     background: transparent;
